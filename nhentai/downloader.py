fd211dbdd731a8487d22d513337af3dd82db19dc
MedicBehindYou <byy0url3ave@gmail.com>
2024-03-24 02:12:40 -0500

    Adding Cookies and User Agent spots for nhentai

---
+++ nhentai/downloader.py
@@ -0,0 +1,138 @@
+import requests
+from pathlib import Path
+from nhentai import utils
+from itertools import repeat
+import multiprocessing as mp
+import time
+import config_loader
+import sqlite3
+import utilities
+import json
+import os
+
+
+def album_page_query(album_id):
+    config = config_loader.load_config()
+    if config:
+        DATABASE_DB = (config['General']['database_db'])
+        LOG_TXT = (config['General']['log_txt'])  
+        cookies = (config['Nhentai']['cookies'])  
+        USER_AGENT = (config['Nhentai']['user_agent'])  
+    else:
+        log('Configuration not loaded.')
+        sys.exit()
+    cookies = json.loads(cookies)
+    headers = {'User-Agent': USER_AGENT}
+
+    query = f'https://nhentai.net/api/gallery/{album_id}'
+    response = requests.get(query, headers=headers, cookies=cookies)
+    data = response.json()
+    data = data['images']
+    i = 1
+    pages = []
+    for page in data['pages']:
+        pages.append((i, page['t']))
+        i+=1
+    return pages
+
+def page_downloader(album_id: str, page: str, name: str, tag: str, media_id: str, base_path: Path = '/app/downloads/nhentai', retries: int = 5):
+    config = config_loader.load_config()
+    if config:
+        DATABASE_DB = (config['General']['database_db'])
+        LOG_TXT = (config['General']['log_txt'])  
+        cookies = (config['Nhentai']['cookies'])  
+        USER_AGENT = (config['Nhentai']['user_agent'])  
+    else:
+        log('Configuration not loaded.')
+        sys.exit()
+    cookies = json.loads(cookies)
+    headers = {'User-Agent': USER_AGENT}    
+    page_num = str(page[0])
+    if page[1] == 'j':
+        ext = '.jpg'
+    elif page[1] == 'p':
+        ext = '.png'
+    elif page[1] == 'g':
+        ext = '.gif'
+    else:
+        print(f'Page {page_num} unrecongnized ext: {page[1]}')
+        return
+    file_name = str(page_num) + str(ext)
+    query = f'https://i.nhentai.net/galleries/{media_id}/{file_name}'
+    base_path = Path(base_path)
+    album_id = str(album_id)
+    album_folder = '[' + album_id + '] ' + name
+    file_path = Path.joinpath(base_path, tag, album_folder, file_name)
+    while len(str(file_path)) > 256:
+        file_path = file_path[:-1]
+    try:
+        if not Path.exists(file_path):  
+            print(f'Starting download of {album_folder} / {file_name}.')
+            retry = 0
+            response = requests.get(query, stream=True, timeout=30, headers=headers, cookies=cookies)              
+            while response.status_code != 200 and retry <= retries:
+                response = requests.get(query, stream=True, timeout=30, headers=headers, cookies=cookies)
+                retry += 1
+            if retry > retries:
+                print(f'[ERROR] Retries reached for {file_name}.')
+                return
+            if len(response.content) > 0:
+                with file_path.open('wb') as image:
+                    image.write(response.content)
+                    print(f'Download of {file_name} completed.')
+    except Exception as e:
+        print(f'[ERROR] Download for {file_name} failed: {e}')
+
+def album_downloader(album_id: str, media_id: str, name:str, tag: str, threads: int = 4, delay: int = 0):
+    config = config_loader.load_config()
+    if config:
+        DATABASE_DB = (config['General']['database_db'])
+        LOG_TXT = (config['General']['log_txt'])      
+    else:
+        log('Configuration not loaded.')
+        sys.exit()
+
+    conn = sqlite3.connect(DATABASE_DB, timeout=20)
+    cursor = conn.cursor()
+
+    pages = album_page_query(album_id)
+    album_id = str(album_id)
+    album_folder = '[' + album_id + '] ' + name
+    utilities.acquire_lock(conn)
+    cursor.execute("SELECT EXISTS(SELECT 1 FROM nhentai WHERE album_id = ? LIMIT 1)", (album_id,))  
+    conn.commit()
+    result = cursor.fetchone()[0] 
+    utils.create_folder(tag, album_folder)
+    if result == 0:
+        start_time = time.time()
+        print(f'Starting doujin {name} with a total of {len(pages)} pages.')
+        pool = mp.Pool(threads)
+        pool.starmap(page_downloader, zip(repeat(album_id), pages, repeat(name), repeat(tag), repeat(media_id)))
+        end_time = time.time()
+        print(f'Finished {name} in {time.strftime("%H:%M:%S", time.gmtime(end_time - start_time))}')
+        utilities.acquire_lock(conn)
+        cursor.execute("INSERT INTO nhentai (album_id, tags) VALUES (?, ?)", (album_id, tag))
+        conn.commit()
+    elif result == 1:
+        utilities.acquire_lock(conn)
+        cursor.execute("SELECT tags FROM nhentai WHERE album_id = ?", (album_id,))
+        conn.commit()
+        album_tags = cursor.fetchone()
+        sep_tags = album_tags[0].split(',')
+        source_path = '/app/downloads/nhentai/' + sep_tags[0] + "/" + album_folder
+        link_path = '/app/downloads/nhentai/' + tag + "/" + album_folder
+        if source_path != link_path:
+            for root, dirs, files in os.walk(source_path):
+                for file in files:
+                    source_dir = os.path.join(root, file)
+                    target_dir = os.path.join(link_path, file)
+                    os.link(source_dir, target_dir)
+        add_tag = "," + tag
+        utilities.acquire_lock(conn)
+        cursor.execute("UPDATE nhentai SET tags = tags || ? WHERE album_id = ?", (add_tag, album_id))
+        conn.commit()
+        print(f'Linked {name} from {sep_tags[0]}.')
+
+    if delay:
+        time.sleep(delay)   
+    return 0 

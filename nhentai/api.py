ab89ec682a4f3fac938065193989f98f40b53ef5
MedicBehindYou <byy0url3ave@gmail.com>
2024-03-27 01:09:25 -0500

    Implemented error handling to catch issues.

--- nhentai/api.py
+++ nhentai/api.py
@@ -1,11 +1,11 @@
 import requests
 import config_loader
 import json
+import time
 
 def fetch_albums(tag: str, sort: str = 'all_time'):
     page = 1
     nhentai_IDs = []
-#    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'}
     config = config_loader.load_config()
     if config:
         DATABASE_DB = (config['General']['database_db'])
@@ -18,17 +18,38 @@ def fetch_albums(tag: str, sort: str = 'all_time'):
         sys.exit()
     cookies = json.loads(cookies)
     headers = {'User-Agent': USER_AGENT}
+    retries = 5
+    retry = 0
+    errors = 0
     while True:
-        query = f'https://nhentai.net/api/galleries/search?query={tag}&sort={sort}&page={page}'
-        print(f'Looking for Albums on page {page}.')
-        response = requests.get(query, headers=headers, cookies=cookies)
-        data = response.json()
-        max_pages = data['num_pages']
-        if data['num_pages'] > page or data['result'] == []:
-            break
-        for album in data['result']:
-            nhentai_IDs.append((album['title']['pretty'], album['id'], album['media_id']))
-            if int(max_page_cap) <= page:
+        try:
+            query = f'https://nhentai.net/api/galleries/search?query={tag}&sort={sort}&page={page}'
+            print(f'Looking for Albums on page {page}.')
+            response = requests.get(query, headers=headers, cookies=cookies)
+            while response.status_code != 200 and retry <= retries:
+                time.sleep(2.5)
+                response = requests.get(query, headers=headers, cookies=cookies)
+                retry += 1
+            if retry > retries:
+                print('[ERROR] Retry Limit reached.')
+                page += 1
+                retry = 0
+            else:
+                data = response.json()
+                max_pages = data['num_pages']
+                if data['num_pages'] < page or data['result'] == []:
+                    return nhentai_IDs
+                for album in data['result']:
+                    nhentai_IDs.append((album['title']['pretty'], album['id'], album['media_id']))
+                    if int(max_page_cap) <= page:
+                        print(f'User set max pages reached.')                      
+                        return nhentai_IDs
+                page += 1
+        except Exception as e:
+            print(f'[ERROR] Query {query} failed with exception: {e}')
+            page += 1
+            errors += 1
+            if errors >= 20:
+                print(f'[ERROR ]Reached Max Error Limit for {tag}')
                 break
-        page += 1
     return nhentai_IDs

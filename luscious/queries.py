5cee0afba38cec9d094da3ea0b7d4a5b1f4461f3
MedicBehindYou <byy0url3ave@gmail.com>
2024-03-28 22:43:08 -0500

    Licensing baby

--- luscious/queries.py
+++ luscious/queries.py
@@ -1,3 +1,19 @@
+#    luster
+#    Copyright (C) 2024  MedicBehindYou
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 def artist_search_query(artist_query: str, page: int, search_query: str = '', sorting: str = 'rating_all_time') -> dict:
         """
         Build Album search query.
